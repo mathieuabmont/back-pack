444bd1cb67be656739eff000ee0e096150794e11
mathieu abmont <mathieu.abmont@gmail.com>
2017-12-05 12:30:13 +0100

    step model done, step route done, step controller partially done

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :trips, except: [:show] do
-    resources :steps, except: [:show]
+    resources :steps, only: [:new, :create]
   end
+  resources :steps, only: [:index, :edit, :update, :destroy]
 end
