a0843e0d1ce966a751859a987b235aff67f6507b
Alix de Bricourt <alix.debricourt@gmail.com>
2017-12-05 12:08:15 +0100

    Trips fini sans les vues

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,19 +1,39 @@
 class TripsController < ApplicationController
   def index
+    @trips = Trip.all
   end
 
   def new
+    @trip = Trip.new
   end
 
   def create
+    @trip = Trip.new(trip_params)
+    if @trip.save
+      # redirect_to index de steps_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @trip = Trip.find(params[:id])
   end
 
   def update
+    @trip = Trip.find(params[:id])
+    @trip.update(params[:trip])
   end
 
   def destroy
+    @trip = Trip.find(params[:id])
+    @trip.destroy
+    redirect_to trips_path
+  end
+
+  private
+
+  def trip_params
+    params.require(:trip).permit(:title)
   end
 end
