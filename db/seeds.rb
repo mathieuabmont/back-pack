ad73b76f773fbcab30a776f4341bfd3c297f8712
Sonialanglet <slanglet@loeildumarketing.com>
2017-12-08 13:16:43 +0100

    seed ok

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,6 +23,11 @@ trip_rachel = Trip.new(title: "France")
 trip_rachel.save!
 p "trip - done"
 
+p "creating lien entre user et trip"
+itinerary_rachel = Itinerary.new(trip: trip_rachel, user: user_rachel)
+itinerary_rachel.save!
+p"itineray done"
+
 
 p "creating 2 steps"
 step_1_rachel = Step.new(title: "Lyon", trip: trip_rachel)
@@ -33,27 +38,25 @@ p "2 steps - done"
 
 
 
-# p "creating 1 transport pour Lyon"
-# transport_1_lyon = Transport.new(departure_date: "2017/27/12", departure_location: "Paris", departure_time: "15h12", arrival_location: "Lyon", arrival_date: "2017/27/12", arrival_time: "19H12", step: step_1_rachel )
-# activity_1_lyon.save!
-# p "1 transport - done"
+p "creating 1 transport pour Lyon"
+transport_1_lyon = Transport.new(departure_date: "2017-27-12", departure_location: "Paris", departure_time: "15h12", arrival_location: "Lyon", arrival_date: "2017-27-12", arrival_time: "19H12", step: step_1_rachel )
+transport_1_lyon.save!
+p "1 transport - done"
 
 
-# p "creating 1 ticket pour Lyon"
-# ticket_1_lyon = Ticket.new.remote_photo_url = "http://res.cloudinary.com/alumni/image/upload/v1512726684/billettrain_c76aiz.jpg"
-# ticket_1_lyon.save!
-# p "1 ticket - done"
+p "creating 1 ticket pour Lyon"
+ticket_1_lyon = Ticket.new.remote_photo_url = "http://res.cloudinary.com/alumni/image/upload/v1512726684/billettrain_c76aiz.jpg"
+ticket_1_lyon.save!
+p "1 ticket - done"
 
 
 
- # p "creating 2 activities pour Lyon"
- # activity_1_lyon = Activity.new(title: "Vieux lyon", url:"http://www.blog-in-lyon.fr", step: step_1_rachel)
- # #activity_1_lyon.remote_photo_url = "http://res.cloudinary.com/alumni/image/upload/v1512496157/vieux_lyon_sarcgv.jpg"
- # activity_1_lyon.save!
- # activity_2_lyon = Activity.new(title: "Basilique Notre Dame", url:"https://lyon.citycrunch.fr", step: step_1_rachel )
- # #activity_2_lyon.remote_photo_url = "http://res.cloudinary.com/alumni/image/upload/v1512496966/Notre_Dame_de_Fourvi%C3%A8re_ksiyml.jpg"
- # activity_2_lyon.save!
- # p "2 activities - done"
+ p "creating 2 activities pour Lyon"
+ activity_1_lyon = Activity.new(title: "Vieux lyon", url:"http://www.blog-in-lyon.fr", photo: "http://a401.idata.over-blog.com/600x446/1/83/03/55/album-nature/2-album-nature/4-ALBUM-NATURE/8-a--CATHEDRALE-ST-JEAN-VIEUX-LYON-ET-VUE-BASILIQUE-FOURVIE.jpg", step: step_1_rachel)
+ activity_1_lyon.save!
+ activity_2_lyon = Activity.new(title: "Basilique Notre Dame", url:"https://lyon.citycrunch.fr", photo: "http://a401.idata.over-blog.com/600x446/1/83/03/55/album-nature/2-album-nature/4-ALBUM-NATURE/8-a--CATHEDRALE-ST-JEAN-VIEUX-LYON-ET-VUE-BASILIQUE-FOURVIE.jpg",step: step_1_rachel )
+ activity_2_lyon.save!
+ p "2 activities - done"
 
 
 
