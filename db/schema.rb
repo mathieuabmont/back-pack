dacdc06057e346c508578faf7807c4d214d3e7bd
mathieu abmont <mathieu.abmont@gmail.com>
2017-12-11 14:40:00 +0100

    front de l'index de steps ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206105019) do
+ActiveRecord::Schema.define(version: 20171211095846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20171206105019) do
     t.bigint "trip_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.index ["trip_id"], name: "index_steps_on_trip_id"
   end
 
