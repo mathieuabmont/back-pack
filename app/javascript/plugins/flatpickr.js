1b0c450f1e443056bcf0c2f7769dc0fa3cd935de
edouard duverger <edouard.courtois.duverger@gmail.com>
2017-12-12 20:36:36 +0100

    change in the view

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -3,16 +3,17 @@ import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
 flatpickr(".datepicker", {
-  altFormat: 'd-m-Y'
+  disableMobile: "true"
 })
 
+
 flatpickr("#transport_departure_date", {
- altFormat: 'd-m-Y',
+ disableMobile: "true",
   // plugins: [new rangePlugin({ input: "#transport_arrival_date"})]
 })
 
 flatpickr("#accommodation_arrival_date", {
-  altFormat: 'd-m-Y',
+  disableMobile: "true",
   plugins: [new rangePlugin({ input: "#accommodation_departure_date"})]
 }
 )
