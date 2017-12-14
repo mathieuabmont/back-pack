f41268708c4cd0a7afbdbe0fc91ad6874914fef4
mathieu abmont <mathieu.abmont@gmail.com>
2017-12-14 15:43:18 +0100

    changé la taille de la date et le pb sur le delete

--- app/models/transport.rb
+++ app/models/transport.rb
@@ -1,6 +1,6 @@
 class Transport < ApplicationRecord
   belongs_to :step
-  has_one :ticket
+  has_one :ticket, dependent: :destroy
 
   validates :departure_date, presence: true
   validates :arrival_date, presence: true
